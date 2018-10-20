51f71c686e079f5cad30541b80245fbcfae723de
m.sawastian <m.sawastian@gmail.com>
2018-10-20 22:17:59 +0200

    Exercise 9-4, 9-5.

--- restaurant.py
+++ restaurant.py
@@ -3,12 +3,19 @@ class Restaurant:
     def __init__(self, name, cuisine):
         self.name = name
         self.cuisine = cuisine
+        self.number_served = 0
 
     def describe_restaurant(self):
         print(self.name.title() + ' serves ' + self.cuisine.title() + ' food.')
 
     def open_restaurant(self):
         print(self.name.title() + ' is now open')
+
+    def set_numbers_server(self, number_served):
+        self.number_served = number_served
+
+    def increment_number_served(self, number_served):
+        self.number_served += number_served
 
 
 mykonos = Restaurant('mykonos', 'greek')
@@ -21,3 +28,10 @@ dolce_vita.describe_restaurant()
 dolce_vita.open_restaurant()
 fat_bob.describe_restaurant()
 fat_bob.open_restaurant()
+print(mykonos.number_served)
+mykonos.number_served = 1
+print(mykonos.number_served)
+mykonos.set_numbers_server(2)
+print(mykonos.number_served)
+mykonos.increment_number_served(1)
+print(mykonos.number_served)
--- user.py
+++ user.py
@@ -3,12 +3,19 @@ class User:
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
+        self.login_attempts = 0
 
     def describe_user(self):
         print("This user's name is: " + self.first_name.title() + ' ' + self.last_name.title() + '.')
 
     def greet_user(self):
         print('Hello ' + self.first_name.title() + ' ' + self.last_name.title() + '!')
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
 
 
 john_smith = User('john', 'smith')
@@ -18,3 +25,10 @@ john_smith.describe_user()
 john_smith.greet_user()
 jane_doe.describe_user()
 jane_doe.greet_user()
+john_smith.increment_login_attempts()
+john_smith.increment_login_attempts()
+john_smith.increment_login_attempts()
+john_smith.increment_login_attempts()
+print(john_smith.login_attempts)
+john_smith.reset_login_attempts()
+print(john_smith.login_attempts)
