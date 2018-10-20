f36710de18d7f878b5e55212048b2c04db2f3b7b
m.sawastian <m.sawastian@gmail.com>
2018-10-20 12:36:25 +0200

    Dog class. Now with methods.

--- dog.py
+++ dog.py
@@ -9,3 +9,11 @@ class Dog():
 
     def roll_over(self):
         print(self.name.title() + ' rolled over!')
+
+
+my_dog = Dog('willie', 3)
+
+print("My dog's name is " + my_dog.name.title() + '.')
+print('My dog is ' + str(my_dog.age) + ' years old.')
+my_dog.sit()
+my_dog.roll_over()
